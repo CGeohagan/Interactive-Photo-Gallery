8f2c8ca499309780a72b6b42c9197e1d86614a28
Colleen Geohagan <ckbowker@gmail.com>
2016-04-10 14:48:59 -0400

    Working on the previous/next buttons. Not able to get them working yet.

--- js/gallery.js
+++ js/gallery.js
@@ -3,9 +3,16 @@
 
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
-var $leftArrow = $('<button type="button" class="fa fa-chevron-circle-left"></button>');
-var $rightArrow = $('<button type="button" class="fa fa-chevron-circle-right"></button>');
+var $leftArrow = $('<span class="leftArrow fa fa-chevron-circle-left"></span>');
+var $rightArrow = $('<span class="rightArrow fa fa-chevron-circle-right"></span>');
 var $caption = $("<p></p>");
+var $selectedImage;
+var imageLocation;
+var captionText;
+
+
+//Add image to overlay
+$overlay.append($image);
 
 //Add left arrow to overlay
 $overlay.append($leftArrow);
@@ -13,75 +20,69 @@ $overlay.append($leftArrow);
 //Add right arrow to overlay
 $overlay.append($rightArrow);
 
-//Add image to overlay
-$overlay.append($image);
-
 //A caption to overlay
 $overlay.append($caption);
 
 //Add overlay.
 $("body").append($overlay);
 
-//Create function for adding the image to the overlay
-var overlayImage = function(imageLocation, captionText) {
-  //Update the image for the overlay
-  $image.attr("src", imageLocation);
-  //Make image alt attribute the caption
-  $caption.text(captionText);
-};
+
+
 
 //Capture the click event on a link to an image.
-
-  $(".photo-thumbnails a").click(function(event){
+  $(".photo-thumbnails a").click(function(event) {
+    $selectedImage = $(this);
     event.preventDefault();
-
-    $(this).addClass("selected");
-  
-    var imageLocation = $(this).attr("href");
-    
-    //Get child's alt attribute 
-    var captionText = $(this).children("img").attr("alt");
-   
-    //Call image overlay function
-    overlayImage(imageLocation, captionText);
-    
+    imageLocation = $(this).attr("href");
+    $image.attr("src", imageLocation);
     //Show the overlay.
     $overlay.show();
+    //Get child's alt attribute 
+    captionText = $(this).children("img").attr("alt");
+    $caption.text(captionText);
   });
    
   
 
-      //When right arrow is clicked
-     $rightArrow.click(function(){
-        //Traverse to next image in gallery
-       var $imageLink = $(".selected").next();
-        //Remove selected class from current picture and add to new picture
-        $(".selected").removeClass("selected");
-        $imageLink.addClass("selected");
-        //Update overlay with new image location
-        imageLocation = $imageLink.attr("href");
-        
-          
-        //Get child's alt attribute 
-       captionText = $imageLink.children("img").attr("alt");
-      
-        //Call image overlay function
-       overlayImage(imageLocation, captionText);
-        
-      //Show the overlay.
-      $overlay.show();
-  
-});  
+ //Function for when right arrow is clicked
+function nextImage(){
+    //Traverse to next image in gallery
+    $selectedImage = $selectedImage.next();
+    //Update overlay with new image location
+    imageLocation = $selectedImage.attr("href");
+    //Update the image for the overlay
+    $image.attr("src", imageLocation);      
+    //Get child's alt attribute 
+    captionText = $selectedImage.children("img").attr("alt");
+    //Make image alt attribute the caption
+    $caption.text(captionText);  
+} 
+
+//Function for when previous arrow is clicked
+function prevImage(){
+    //Traverse to previous image in gallery
+    $selectedImage = $selectedImage.prev();
+    //Update overlay with new image location
+    imageLocation = $selectedImage.attr("href");
+    //Update the image for the overlay
+    $image.attr("src", imageLocation);      
+    //Get child's alt attribute 
+    captionText = $selectedImage.children("img").attr("alt");
+    //Make image alt attribute the caption
+    $caption.text(captionText);  
+} 
+
+
+
+$(".rightArrow").click(function(){
+  nextImage();
+});
+
+$(".leftArrow").click(function(){
+  prevImage();
+});
 
 
 
 
 
-
-
-//When left arrow is clicked
-
-  //Traverse to previous image in gallery
-
-
-//When overlay is clicked
